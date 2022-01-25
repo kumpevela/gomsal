9b6222afa978fde7c87d5d0f9a2fac2d6b00a612
steve kumpevela <kumpevela@gmail.com>
2022-01-25 11:21:40 +0100

    correcciones ortograficas

--- src/app/pages/home/components/home-content/home-content.component.ts
+++ src/app/pages/home/components/home-content/home-content.component.ts
@@ -20,7 +20,7 @@ export class HomeContentComponent implements OnInit {
         alt: 'Gomsal'
       }, 
       titleDescription: 'Amor al arte del baile',
-      description: 'El baile es un arte donde se utiliza el movimiento del cuerpo, omo una forma de expresión y de interacción social con fines de entretenimiento, artísticos y reproductivos. La danza también es una forma de comunicación. Utilizamos el lenjuage no verbal entre nosotras para expresar sentimientos y emociones atraves de gestos y movimientos'
+      description: 'El baile es un arte donde se utiliza el movimiento del cuerpo, como una forma de expresión y de interacción social con fines de entretenimiento, artísticos y reproductivos. La danza también es una forma de comunicación. Utilizamos el lenjuage no verbal entre nosotras para expresar sentimientos y emociones a través de gestos y movimientos.'
     }
     this.dnc = {
       title: 'Nuestras bailarinas', 
@@ -29,7 +29,7 @@ export class HomeContentComponent implements OnInit {
         alt: 'Bailarinas'
       }, 
       titleDescription: 'Somos bailarinas',
-      description: 'Con mas de 10 años de historia, nos alegra ir de ciudad en ciudad monstrando nuestro espectaculo, haciendo vibrar al publico con nuestros saltos y giros, ya que para nosotras mas que un trabajo es una afición de la que podemos vivir'
+      description: 'Con más de 10 años de historia, nos alegra ir de ciudad en ciudad monstrando nuestro espectáculo, haciendo vibrar al publico con nuestros saltos y giros, ya que para nosotras más que un trabajo es una pasión de la que podemos vivir.'
     }
     this.show = {
       title: 'Proximos conciertos', 
@@ -38,7 +38,7 @@ export class HomeContentComponent implements OnInit {
         alt: 'Espectaculos'
       }, 
       titleDescription: 'Nuestros temas musicales',
-      description: 'Nuestros espectaculos van cambiando trimestral mente y en esta ocasión nos complace presentarles, El Rey León, La Cenicienta y para el publico infantil Toy Story. Cada espectaculo tiene una duración de 90 minutos, con un descanso de 15 minutos en mitad del espectaculo, excepto para el de Toy Story que tendrá 2 descansos de 20 minutos para que el publico mas infantil salga a descansar'
+      description: 'Nuestros espectáculos van cambiando trimestral mente y en esta ocasión nos complace presentarles, El Rey León, La Cenicienta y para el público infantil Toy Story. Cada espectáculo tiene una duración de 90 minutos, con un descanso de 15 minutos en mitad del espectáculo, excepto para el de Toy Story que tendrá 2 descansos de 20 minutos para que el público más infantil salga a descansar.'
     }
   }
 
